84f9168e905e2b48828b14744700ee19ffbef050
Kirill Kolesnichenko <k.kolesnichenko@digital-sector.ru>
2025-03-16 13:13:38 +0300

    feat|front: exchange правки контента продуктов

--- pugo-front/src/modules/Exchange/exchange.content.ts
+++ pugo-front/src/modules/Exchange/exchange.content.ts
@@ -1,16 +1,16 @@
 export const products = {
-	50: { stars: 50, pugo: 2500, description: '1000 PUGO for 50 Stars ⭐' },
-	150: { stars: 150, pugo: 10000, description: '1750 PUGO for 75 Stars ⭐' },
-	500: { stars: 500, pugo: 50000, description: '2500 PUGO for 100 Stars ⭐' },
+	50: { stars: 50, pugo: 2500, description: '2500 PUGO for 50 Stars ⭐' },
+	150: { stars: 150, pugo: 10000, description: '10000 PUGO for 150 Stars ⭐' },
+	500: { stars: 500, pugo: 50000, description: '50000 PUGO for 500 Stars ⭐' },
 	1000: {
 		stars: 1000,
 		pugo: 150000,
-		description: '3500 PUGO for 150 Stars ⭐',
+		description: '150000 PUGO for 1000 Stars ⭐',
 	},
 	2500: {
 		stars: 2500,
 		pugo: 500000,
-		description: '7770 PUGO for 250 Stars ⭐',
+		description: '500000 PUGO for 2500 Stars ⭐',
 	},
 	1: {
 		for_admin: true,
