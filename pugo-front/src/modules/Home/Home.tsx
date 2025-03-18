0101b5de294ab834e29486523e61764f1b25e293
Kirill Kolesnichenko <k.kolesnichenko@digital-sector.ru>
2025-03-18 23:04:55 +0300

    feat|front: правки

--- pugo-front/src/modules/Home/Home.tsx
+++ pugo-front/src/modules/Home/Home.tsx
@@ -7,11 +7,10 @@ import Button from '@/components/UI/Button/Button'
 import { HomeProps } from './Home.d'
 
 import { styled } from '@mui/material/styles'
-import FormGroup from '@mui/material/FormGroup'
+
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Switch, { SwitchProps } from '@mui/material/Switch'
-import Stack from '@mui/material/Stack'
-import Typography from '@mui/material/Typography'
+
 import {
 	HomeStyled,
 	BannerStyled,
@@ -177,11 +176,7 @@ export const Home: FunctionComponent<HomeProps> = ({ data, children }) => {
 						</NDXStyled>
 					</CoinCountInfo>
 				</CoinCountInfoWrapper>
-				<Coin
-					tokens={displayingTokens}
-					setTokens={setDisplayingTokens}
-					onActivity={resetInactivityTimer}
-				/>
+				<Coin />
 				<ActivityWrapper>
 					<CoinName>MAJESTIC</CoinName>
 					<AutoMining>
